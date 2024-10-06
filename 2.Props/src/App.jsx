1176b03c4f9dacc9243aab6321ce4dc613198996
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-10-06 11:07:00 +0545

    new

--- 2.Props/src/App.jsx
+++ 2.Props/src/App.jsx
@@ -1,10 +1,24 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 function App() {
-  
+  // Can only return 1 element
+
   return (
     <>
-      This is React App
+      <Navbar/>
+
+      <main>
+        This is the main content.
+      </main>
+
+      <div className="cards">
+        
+      </div>
+
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
