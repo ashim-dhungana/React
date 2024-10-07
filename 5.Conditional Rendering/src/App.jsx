29da06d7ea381df9daa5a6c1a7ca1715f1e5fc50
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-10-07 10:44:35 +0545

    new

---
+++ 5.Conditional Rendering/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [showbtn, setshowbtn] = useState(false)
+
+  return (
+    <>
+
+      {/* If showbtn is True then display */}
+      {showbtn && <button>Another button clicked</button>}
+
+      <br/>
+      <br/>   
+
+      {/* For both True and False condition */}
+      {showbtn ? <button>showbtn is True</button> : <button>showtbn is False</button>}
+      
+      <div className="card">
+
+        <button onClick={()=> setshowbtn(!showbtn)}>Toggle button</button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
