6925f00b97c0cc2b88808672b98dfebcf08b3fb8
ashimdhungana07 <ashimdhungana6@gmail.com>
2024-10-12 17:53:46 +0545

    new

---
+++ 7.Routing/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Login</li>
+    </ul>
+  );
+};
+
+export default Navbar;
